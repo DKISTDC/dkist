ff29d97a093d55263a35d9deb015cb4e0e5c2c7a
Stuart Mumford <stuart@cadair.com>
2018-11-26 17:51:58 +0000

    change ds import

--- examples/create_dataset.py
+++ examples/create_dataset.py
@@ -5,13 +5,13 @@ Loading a Dataset
 This example demonstrates the `dkist.Dataset` class.
 """
 
-import dkist
+import dkist.dataset
 from dkist.data.sample import EIT_DATASET
 
 ###############################################################################
 # Dataset objects are created from a directory containing one asdf file and
 # many FITS files. Here we use a test dataset made from EIT images.
-ds = dkist.Dataset.from_directory(EIT_DATASET)
+ds = dkist.dataset.Dataset.from_directory(EIT_DATASET)
 
 ###############################################################################
 # The dataset comprises of a `dask.Array` object and a `gwcs.WCS` object. The
