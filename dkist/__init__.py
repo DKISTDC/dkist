4bc8c266a1ab4dc0380ecbec184b95bf751bd4a8
Stuart Mumford <stuart@cadair.com>
2019-01-31 14:35:35 +0000

    linter

--- dkist/__init__.py
+++ dkist/__init__.py
@@ -1,8 +1,6 @@
-import sys
-
 from pkg_resources import get_distribution, DistributionNotFound
 
-from ._dkist_init import *
+from ._dkist_init import *  # noqa
 
 try:
     __version__ = get_distribution(__name__).version
@@ -10,8 +8,6 @@ except DistributionNotFound:
     # package is not installed
     __version__ = "unknown"
 
-if not _ASTROPY_SETUP_:
+if not _ASTROPY_SETUP_:  # noqa
     # For egg_info test builds to pass, put package imports here.
-    from .dataset import Dataset
-
-
+    from .dataset import Dataset  # noqa
