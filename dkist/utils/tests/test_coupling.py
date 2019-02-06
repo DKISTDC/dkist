af1476e25b82383ac48131acf69ba613b4c5a80f
Stuart Mumford <stuart@cadair.com>
2019-02-06 16:44:16 +0000

    Prototype a coupling matrix

---
+++ dkist/utils/tests/test_coupling.py
@@ -0,0 +1,23 @@
+from dkist.utils.model_coupled import coupling_matrix
+
+import numpy as np
+import pytest
+from astropy.modeling.models import Shift, Identity, Pix2Sky_AZP, Pix2Sky_CylindricalPerspective
+
+
+@pytest.mark.parametrize("model", ((Pix2Sky_CylindricalPerspective()),
+                                   (Pix2Sky_AZP())))
+def test_invaritant_separable_model(model):
+    sep = coupling_matrix(model)
+
+    assert sep.all()
+
+
+def test_compound_model():
+    comp = coupling_matrix(Pix2Sky_CylindricalPerspective() & (Identity(1) | Shift(10)))
+
+    answer = np.array([[True, True, False],
+                       [True, True, False],
+                       [False, False, True]])
+
+    np.testing.assert_array_equal(comp, answer)
