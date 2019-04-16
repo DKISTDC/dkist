78146881b56f1fbc24d4a91b2fbc1c4a3faa3ec1
Stuart Mumford <stuart@cadair.com>
2019-04-16 10:47:49 +0100

    Fix import order

--- dkist/io/__init__.py
+++ dkist/io/__init__.py
@@ -1,12 +1,12 @@
+import dkist.config as _config
+
 from .fits import AstropyFITSLoader, BaseFITSLoader
-from .reference_collections import (BaseFITSArrayContainer, DaskFITSArrayContainer,
+from .reference_collections import (BaseFITSArrayContainer,
+                                    DaskFITSArrayContainer,
                                     NumpyFITSArrayContainer)
 
 __all__ = ['BaseFITSLoader', 'AstropyFITSLoader', 'BaseFITSArrayContainer',
            'NumpyFITSArrayContainer', 'DaskFITSArrayContainer', 'conf']
-
-
-import dkist.config as _config
 
 
 class Conf(_config.ConfigNamespace):
