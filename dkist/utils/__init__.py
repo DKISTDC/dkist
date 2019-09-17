74873ac5a0b69b6c00a7b04417e7e422eedf093e
Stuart Mumford <stuart@cadair.com>
2019-09-17 13:28:18 +0100

    Import in_notebook from parfive

--- dkist/utils/__init__.py
+++ dkist/utils/__init__.py
@@ -3,19 +3,4 @@
 # This sub-module is destined for common non-package specific utility
 # functions.
 
-
-def in_notebook():  # pragma: no cover
-    """
-    Attempts to detect if this python process is connected to a Jupyter Notebook.
-    """
-    try:
-        import ipykernel.zmqshell
-        shell = get_ipython()  # noqa
-        if isinstance(shell, ipykernel.zmqshell.ZMQInteractiveShell):
-            # Check that we can import the right widget
-            from tqdm import _tqdm_notebook
-            _tqdm_notebook.IntProgress
-            return True
-        return False
-    except Exception:
-        return False
+from parfive.utils import in_notebook
