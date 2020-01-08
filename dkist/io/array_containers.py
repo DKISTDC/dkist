b593ca4961467a7af9c16a00b7b6b01177714c74
Stuart Mumford <stuart@cadair.com>
2020-01-08 10:33:21 +0000

    Initial container stub

--- dkist/io/array_containers.py
+++ dkist/io/array_containers.py
@@ -3,6 +3,7 @@ This submodule provides tools for resolving arrays of
 `asdf.ExternalArrayReference` objects to Python array-like objects.
 """
 import abc
+import collections
 from functools import partial
 
 import dask.array as da
@@ -27,6 +28,65 @@ common_parameters = """
     kwargs : `dict`
         Extra keyword arguments are passed to the loader class.
 """
+
+
+class ExternalArrayReferenceCollection(collections.abc.Sequence):
+    """
+    A homogeneous collection of `asdf.ExternalArrayReference` like objects.
+
+    This class differs from a list of `asdf.ExternalArrayReference` objects
+    because all of the references have the same shape, dtype and target. This
+    allows for much more yaml and schema efficient storage in the asdf tree.
+    """
+
+    @classmethod
+    def _validate_homogenaity(cls, shape, target, dtype, ear):
+        if isinstance(ears, (list, tuple)):
+            return list(map(partial(cls._validate_homogenaity, shape, target, dtype), ear))
+
+        if not isinstance(ear, ExternalArrayReference):
+            raise TypeError("Every element of must be an instance of ExternalArrayReference")
+        if ear.dtype != dtype:
+            raise ValueError(f"The Reference {ear} does not have the same dtype as the first reference")
+        if ear.shape != shape:
+            raise ValueError(f"The Reference {ear} does not have the same shape as the first reference")
+        if ear.target != target:
+            raise ValueError(f"The Reference {ear} does not have the same target as the first reference")
+
+        return ear.fileuri
+
+    @classmethod
+    def from_external_array_references(cls, ears):
+        shape = ears[0].shape
+        dtype = ears[0].dtype
+        target = ears[0].target
+
+        for i, ele in enumerate(ears):
+            uris = cls._validate_homogenaity(shape, target, dtype, ears)
+
+        return cls(uris, target, dtype, shape)
+        
+
+    def __init__(self, fileuris, target, dtype, shape):
+        self.shape = tuple(shape)
+        self.dtype = dtype
+        self.target = target
+        self.fileuris = fileuris
+
+    def _to_ears(self, urilist):
+        if isinstance(urilist, (list, tuple)):
+            return list(map(self._to_ears, urilist))
+        return asdf.ExternalArrayReference(fileuri, self.target, self.dtype, self.shape)
+
+    @property
+    def external_array_references(self):
+        return self._to_ears(self.urilist)
+
+    def __getitem__(self, item):
+        return type(self)(self.fileuris[item], self.target, self.dtype, self.shape)
+
+    def __len__(self):
+        return len(self.fileuris)
 
 
 @add_common_docstring(append=common_parameters)
