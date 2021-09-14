836a66daa5f619a71d479f831220737dd9445297
Stuart Mumford <stuart@cadair.com>
2021-09-14 14:23:03 +0100

    changelog and revert to old theme
    Test rtd.com build

--- docs/conf.py
+++ docs/conf.py
@@ -142,7 +142,7 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "dkist"
+from dkist_sphinx_theme.conf import *
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
