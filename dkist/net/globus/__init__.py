86e71a52345de5c2a0e9bc5a79b5b4d24091002d
Stuart Mumford <stuart@cadair.com>
2021-11-17 11:41:18 +0000

    Update Sim Globus ID

--- dkist/net/globus/__init__.py
+++ dkist/net/globus/__init__.py
@@ -7,7 +7,7 @@ from .auth import *
 from .endpoints import *
 from .transfer import *
 
-DKIST_DATA_CENTRE_ENDPOINT_ID = '0485e4df-cb8b-420f-9113-ad5cd8af304f'  # DKIST Sim Data 2021-04
+DKIST_DATA_CENTRE_ENDPOINT_ID = '433a057f-5ca0-4949-9bb1-c994514d1258'  # DKIST Sim Data 2021-04
 """The Globus endpoint ID of the main DKIST globus endpoint."""
 
 DKIST_DATA_CENTRE_DATASET_PATH = "/{bucket}/{primaryProposalId}/{datasetId}"
