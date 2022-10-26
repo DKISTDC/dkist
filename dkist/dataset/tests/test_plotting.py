c5162cd74df1e1bc842de95e206f1f7078a1ba4c
William Jamieson <wjamieson@stsci.edu>
2022-10-26 11:41:53 -0400

    Fix tests broken by pytest 7.2

--- dkist/dataset/tests/test_plotting.py
+++ dkist/dataset/tests/test_plotting.py
@@ -5,6 +5,7 @@ import pytest
 from astropy.visualization.wcsaxes import WCSAxes
 
 
+@pytest.mark.mpl_image_compare
 @pytest.mark.parametrize("aslice", (np.s_[0, :, :], np.s_[:, 0, :], np.s_[:, :, 0]))
 def test_dataset_projection(dataset_3d, aslice):
     pytest.importorskip("ndcube", "2.0.2")  # https://github.com/sunpy/ndcube/pull/509
@@ -15,7 +16,7 @@ def test_dataset_projection(dataset_3d, aslice):
     return fig
 
 
-# @pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare
 @pytest.mark.parametrize("aslice", (np.s_[0, :, :], np.s_[:, 0, :], np.s_[:, :, 0]))
 def test_2d_plot(dataset_3d, aslice):
     fig = plt.figure()
@@ -23,7 +24,7 @@ def test_2d_plot(dataset_3d, aslice):
     return fig
 
 
-# @pytest.mark.mpl_image_compare
+@pytest.mark.mpl_image_compare
 def test_2d_plot2(dataset_3d):
     fig = plt.figure()
     dataset_3d[:, :, 0].plot(axes_units=["Angstrom", "deg", "deg"])
