ed3793e7ccffcc06decf4673554b2237612167f1
Drew Leonard <andy.j.leonard@gmail.com>
2023-07-19 14:00:24 +0100

    Stop astropy downloading data in a test fixture

--- dkist/conftest.py
+++ dkist/conftest.py
@@ -296,6 +296,8 @@ def small_visp_dataset():
 
 @pytest.fixture(scope="session")
 def large_visp_dataset():
+    # Need this so that astropy doesn't try to download data and kill the tests
+    astropy.utils.iers.conf.auto_download = False
     vispdir = Path(rootdir) / "large_visp"
     ds = SimpleVISPDataset(n_maps=1, n_steps=20, n_stokes=4, time_delta=10,
                            linewave=500*u.nm, detector_shape=(500, 1280))
