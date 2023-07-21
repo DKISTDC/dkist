56968fed259e4b87143184f539d1ec90e9b6d5be
Drew Leonard <andy.j.leonard@gmail.com>
2023-07-21 11:35:38 +0100

    Forgot to commit the actual loader because I'm a fool

---
+++ dkist/dataset/loader.py
@@ -0,0 +1,75 @@
+import importlib.resources as importlib_resources
+from pathlib import Path
+from functools import singledispatch
+
+from jsonschema.exceptions import ValidationError
+
+import asdf
+
+
+@singledispatch
+def load_dataset():
+    pass
+
+
+@load_dataset.register
+def _load_from_string(path: str):
+    return _load_from_path(Path(path))
+
+
+@load_dataset.register
+def _load_from_path(path: Path):
+    path = path.expanduser()
+    if not path.is_dir():
+        if not path.exists():
+            raise ValueError(f"File {path} does not exist.")
+        # elif raise error if extension is not .asdf
+        return _load_from_asdf(path)
+    else:
+        return _load_from_directory(path)
+
+
+def _load_from_directory(directory):
+    """
+    Construct a `~dkist.dataset.Dataset` from a directory containing one
+    asdf file and a collection of FITS files.
+    """
+    base_path = Path(directory).expanduser()
+    if not base_path.is_dir():
+        raise ValueError("directory argument must be a directory")
+    asdf_files = tuple(base_path.glob("*.asdf"))
+
+    if not asdf_files:
+        raise ValueError("No asdf file found in directory.")
+    elif len(asdf_files) > 1:
+        raise NotImplementedError("Multiple asdf files found in this "
+                                    "directory. Use from_asdf to specify which "
+                                    "one to use.")  # pragma: no cover
+
+    asdf_file = asdf_files[0]
+
+    return _load_from_asdf(asdf_file)
+
+
+def _load_from_asdf(filepath):
+    """
+    Construct a dataset object from a filepath of a suitable asdf file.
+    """
+    from dkist.dataset import TiledDataset
+    filepath = Path(filepath).expanduser()
+    base_path = filepath.parent
+    try:
+        with importlib_resources.as_file(importlib_resources.files("dkist.io") / "level_1_dataset_schema.yaml") as schema_path:
+            with asdf.open(filepath, custom_schema=schema_path.as_posix(),
+                            lazy_load=False, copy_arrays=True) as ff:
+                ds = ff.tree['dataset']
+                if isinstance(ds, TiledDataset):
+                    for sub in ds.flat:
+                        sub.files.basepath = base_path
+                else:
+                    ds.files.basepath = base_path
+                return ds
+
+    except ValidationError as e:
+        err = f"This file is not a valid DKIST Level 1 asdf file, it fails validation with: {e.message}."
+        raise TypeError(err) from e
