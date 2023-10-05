dadc91dc8a748896c903c4439af75f9b1673e164
Drew Leonard <andy.j.leonard@gmail.com>
2023-10-05 14:29:13 +0100

    More cropping tests

--- dkist/dataset/tests/test_crop_dataset.py
+++ dkist/dataset/tests/test_crop_dataset.py
@@ -14,3 +14,29 @@ def test_crop_visp_by_only_stokes(croppable_visp_dataset):
         None,
         StokesCoord('I'),
     ])
+
+
+def test_crop_cryo_by_only_stokes(croppable_cryo_dataset):
+    croppable_cryo_dataset.crop([
+        None,
+        None,
+        StokesCoord('I'),
+    ],
+    [
+        None,
+        None,
+        StokesCoord('I'),
+    ])
+
+
+def test_crop_cryo_by_only_time(croppable_cryo_dataset):
+    coords = croppable_cryo_dataset.wcs.pixel_to_world(0, 0, 0, 0, 0), croppable_cryo_dataset.wcs.pixel_to_world(1751, 1888, 1, 1, 3)
+    print(coords)
+    croppable_cryo_dataset.crop(*coords)
+
+
+def test_crop_cryo_by_only_lonlat(croppable_cryo_dataset):
+    coords = croppable_cryo_dataset.wcs.pixel_to_world(0, 0, 0, 0, 0), croppable_cryo_dataset.wcs.pixel_to_world(1000, 1000, 2, 2, 3)
+    print(coords)
+    # Using calculated coords
+    cropped = croppable_cryo_dataset.crop(*coords)
