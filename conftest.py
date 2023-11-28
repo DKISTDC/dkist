f9dee17f06bc79626c77dc9d6e19e7a735ff28ad
Drew Leonard <andy.j.leonard@gmail.com>
2023-11-28 13:30:34 +0000

    Don't hit DKIST search API during tests

--- conftest.py
+++ conftest.py
@@ -8,3 +8,5 @@ def pytest_configure(config):
     # pre-cache the IERS file for astropy to prevent downloads
     # which will cause errors with remote_data off
     from astropy.utils.iers import IERS_Auto; IERS_Auto.open()
+
+    os.environ['DKIST_SKIP_UPDATE_SEARCH_VALUES'] = 'True'
