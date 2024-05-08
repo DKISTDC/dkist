ed3f1aa82f9055b7c56aad7b86695bd269fb08f7
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-08 13:34:11 +0100

    Add a basic plotting benchmark

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -1,6 +1,7 @@
 import gzip
 from pathlib import Path
 import pytest
+import matplotlib.pyplot as plt
 
 from dkist import load_dataset
 from dkist.data.test import rootdir
@@ -17,3 +18,11 @@ def test_load_asdf(benchmark, tmp_path_factory):
         with open(vispdir / "test_visp.asdf", mode="wb") as afo:
             afo.write(gfo.read())
     benchmark(load_asdf, vispdir)
+
+
+@pytest.mark.benchmark
+@pytest.mark.parametrize("axes", [[None, 'x', 'y', None], [None, 'y', 'x', None]])
+def test_plot_dataset(benchmark, axes, large_visp_dataset):
+
+    large_visp_dataset.plot(plot_axes=[None, 'y', 'x', None])
+    plt.savefig('plot_profiling')
