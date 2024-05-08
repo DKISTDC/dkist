dcc8ae79cafc99547afd408fd194d193ec4808f3
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-08 14:22:19 +0100

    Tidying

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -8,17 +8,13 @@ from dkist import load_dataset
 from dkist.data.test import rootdir
 
 
-def load_asdf(vispdir):
-    ds = load_dataset(vispdir / "test_visp.asdf")
-
-
 @pytest.mark.benchmark
 def test_load_asdf(benchmark, tmp_path_factory):
     vispdir = tmp_path_factory.mktemp("data")
     with gzip.open(Path(rootdir) / "large_visp.asdf.gz", mode="rb") as gfo:
         with open(vispdir / "test_visp.asdf", mode="wb") as afo:
             afo.write(gfo.read())
-    benchmark(load_asdf, vispdir)
+    benchmark(load_dataset, vispdir / "test_visp.asdf")
 
 
 @pytest.mark.benchmark
