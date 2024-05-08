96d20c549be2cf92e93b051374ff068f39312e44
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-08 12:57:36 +0100

    Use the actual codspeed flag for pytest

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -1,5 +1,6 @@
 import gzip
 from pathlib import Path
+import pytest
 
 from dkist import load_dataset
 from dkist.data.test import rootdir
@@ -9,6 +10,7 @@ def load_asdf(vispdir):
     ds = load_dataset(vispdir / "test_visp.asdf")
 
 
+@pytest.mark.benchmark
 def test_load_asdf(benchmark, tmp_path_factory):
     vispdir = tmp_path_factory.mktemp("data")
     with gzip.open(Path(rootdir) / "large_visp.asdf.gz", mode="rb") as gfo:
