4fa2e2cb6e21dd7bee35213247261f721299d972
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-08 14:51:06 +0100

    Sure this used to just make up a sensible default but whatever

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -19,4 +19,4 @@ def test_plot_dataset(benchmark, axes, large_visp_dataset):
     @benchmark
     def plot_and_save_fig(ds=large_visp_dataset, axes=axes):
         ds.plot(plot_axes=axes)
-        plt.savefig()
+        plt.savefig("tmpplot")
