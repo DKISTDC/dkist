cd8c55e385fa7c1f130f3874bde4fd189cfa5d08
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-10 11:00:33 +0100

    Add a non-plotting benchmark

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pytest
 
 from dkist import load_dataset
@@ -7,6 +8,20 @@ from dkist import load_dataset
 @pytest.mark.benchmark
 def test_load_asdf(benchmark, large_visp_dataset_file):
     benchmark(load_dataset, large_visp_dataset_file)
+
+
+@pytest.mark.benchmark
+def test_pixel_to_world(benchmark, large_visp_dataset):
+    ds = large_visp_dataset
+    pxcoords = []
+    for size in ds.wcs.pixel_shape:
+        pxcoords.append(np.arange(size))
+
+    pxcoords = np.mgrid[:ds.wcs.pixel_shape[0]:5,
+                        :ds.wcs.pixel_shape[1]:5,
+                        :ds.wcs.pixel_shape[2]:5]
+
+    benchmark(ds.wcs.pixel_to_world_values, *pxcoords, 0)
 
 
 @pytest.mark.benchmark(min_rounds=2)
