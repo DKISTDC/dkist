d581deded9fffb38b02143b2662f4d2c7abae49a
Drew Leonard <andy.j.leonard@gmail.com>
2024-05-15 11:30:29 +0100

    Bring back savefig

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -24,17 +24,13 @@ def test_pixel_to_world(benchmark, large_visp_dataset):
     benchmark(ds.wcs.pixel_to_world_values, *pxcoords, 0)
 
 
-@pytest.mark.benchmark(min_rounds=2)
+@pytest.mark.benchmark(min_rounds=1)
 @pytest.mark.parametrize("axes", [
-    ["y", "x", None, None],
-    ["y", None, "x", None],
-    ["y", None, None, "x"],
     [None, "y", "x", None],
-    [None, "y", None, "x"],
-    [None, None, "y", "x"],
 ])
 def test_plot_dataset(benchmark, axes, large_visp_dataset):
     @benchmark
     def plot_and_save_fig(ds=large_visp_dataset, axes=axes):
         ds.plot(plot_axes=axes)
-    plt.close()
+        plt.savefig("tmpplot")
+        plt.close()
