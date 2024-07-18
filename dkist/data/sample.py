235ed802afdac2e22a8b61174d3af34d67f5d917
Stuart Mumford <stuart@cadair.com>
2024-07-18 15:14:31 +0100

    Add sunpy style sample data downloading

--- dkist/data/sample.py
+++ dkist/data/sample.py
@@ -1,9 +1,27 @@
-import pathlib
+"""
+This module provides some (partial) sample datasets.
+"""
 
-from astropy.io import fits
+from ._sample import _SAMPLE_DATASETS, VISP_HEADER, _get_sample_datasets
 
-__all__ = ["VISP_HEADER"]
+__all__ = ["download_all_sample_data", *sorted(_SAMPLE_DATASETS.keys()), VISP_HEADER]
 
-_data_dir = pathlib.Path(__file__).parent
 
-VISP_HEADER = fits.Header.fromtextfile(_data_dir / "VISP_HEADER.hdr")
+# See PEP 562 (https://peps.python.org/pep-0562/) for module-level __dir__()
+def __dir__():
+    return __all__
+
+
+# See PEP 562 (https://peps.python.org/pep-0562/) for module-level __getattr__()
+def __getattr__(name):
+    if name in _SAMPLE_DATASETS:
+        return _get_sample_datasets(name)[0]
+
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+def download_all_sample_data():
+    """
+    Download all sample data at once that has not already been downloaded.
+    """
+    return _get_sample_datasets(_SAMPLE_DATASETS.keys())
