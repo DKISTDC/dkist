8d46818f6f685854768faea7f4bd06e67857de28
Drew Leonard <andy.j.leonard@gmail.com>
2024-08-29 16:01:34 +0100

    Minor tweaks

--- dkist/dataset/utils.py
+++ dkist/dataset/utils.py
@@ -26,7 +26,7 @@ def dataset_info_str(ds):
     instr = ds.inventory.get("instrumentName", "")
     if instr:
         instr += " "
-    nframes = ds.inventory.get("frameCount", "an unknown number")
+    nframes = len(ds.files)
     dsID = ds.inventory.get("datasetId", "(no DatasetID)")
 
     if is_tiled:
@@ -34,8 +34,10 @@ def dataset_info_str(ds):
     else:
         s = f"This {instr}Dataset {dsID} "
 
-    s += f"has {wcs.pixel_n_dim} pixel and {wcs.world_n_dim} world dimensions and consists of {nframes} frames\n"
+    s += f"has {wcs.pixel_n_dim} pixel and {wcs.world_n_dim} world dimensions"
+
     if ds.files:
+        s += f" and consists of {nframes} frames\n"
         s += f"Files are stored in {ds.files.basepath}\n\n"
     s += f"The data are represented by a {type(ds.data)} object:\n{ds.data}\n\n"
 
