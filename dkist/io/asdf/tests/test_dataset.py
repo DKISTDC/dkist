32131984c93bd22dfe96a650ecb697862a7fbbac
Drew Leonard <andy.j.leonard@gmail.com>
2025-01-21 16:06:04 +0000

    This was breaking things but I'm not mad, you're mad

--- dkist/io/asdf/tests/test_dataset.py
+++ dkist/io/asdf/tests/test_dataset.py
@@ -71,7 +71,7 @@ def test_roundtrip_tiled_dataset(simple_tiled_dataset):
                              "dataset",
                              lf("simple_tiled_dataset"),
                          ],
-                         indirect=True)
+                         indirect=False)
 def test_save_dataset_without_file_schema(tagobj, tmp_path):
     tree = {"dataset": tagobj}
     with asdf.AsdfFile(tree) as afile:
@@ -97,9 +97,9 @@ def test_asdf_tags(dataset, tmp_path):
 @pytest.mark.parametrize("tagobj",
                          [
                              "dataset",
-                             "simple_tiled_dataset",
+                             lf("simple_tiled_dataset"),
                          ],
-                         indirect=True)
+                         indirect=False)
 def test_save_dataset_with_file_schema(tagobj, tmpdir):
     tree = {"dataset": tagobj}
     with importlib_resources.as_file(importlib_resources.files("dkist.io") / "level_1_dataset_schema.yaml") as schema_path:
