8ba86f6adf8d17b3538d06c12bff679b0a785fdf
Drew Leonard <andy.j.leonard@gmail.com>
2025-02-10 14:13:30 +0000

    Same again for benchmarks

--- dkist/tests/test_benchmarks.py
+++ dkist/tests/test_benchmarks.py
@@ -41,6 +41,7 @@ def test_plot_dataset(benchmark, axes, visp_dataset_no_headers, tmp_path):
 
 
 @pytest.mark.benchmark
+@pytest.mark.remote_data
 def test_dataset_compute_data_full_files(benchmark):
     """
     Note that although this will load all the files to compute the data, the
@@ -48,7 +49,8 @@ def test_dataset_compute_data_full_files(benchmark):
     because it doesn't support that. This test therefore only assesses the
     performance of the compute step.
     """
-    from dkist.data.sample import VISP_BKPLX
+    from dkist.data.sample import VISP_BKPLX, download_all_sample_data
+    download_all_sample_data()
     ds = load_dataset(VISP_BKPLX)[0, :15]
     benchmark(ds.data.compute)
 
@@ -56,8 +58,10 @@ def test_dataset_compute_data_full_files(benchmark):
 
 
 @pytest.mark.benchmark
+@pytest.mark.remote_data
 def test_dataset_compute_data_partial_files(benchmark):
-    from dkist.data.sample import VISP_BKPLX
+    from dkist.data.sample import VISP_BKPLX, download_all_sample_data
+    download_all_sample_data()
     ds = load_dataset(VISP_BKPLX)[0, :15, :100, :100]
     benchmark(ds.data.compute)
 
