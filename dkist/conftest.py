6881809db9b57e701eeb3f88c8151884e2723bb8
Drew Leonard <andy.j.leonard@gmail.com>
2025-04-28 11:46:37 +0100

    Couple of test infrastructure tweaks

--- dkist/conftest.py
+++ dkist/conftest.py
@@ -409,8 +409,12 @@ def pytest_runtest_call(item):
             # Replace either the fixture specified as the first arg of the marker, or the first fixture in the test definition
             replace_arg = mark.args[0] if mark.args else item.fixturenames[0]
             if ds:
+                if isinstance(item.funcargs[replace_arg], TiledDataset):
+                    pytest.skip()
                 item.funcargs[replace_arg] = load_dataset(ds)
             if tds:
+                if isinstance(item.funcargs[replace_arg], Dataset):
+                    pytest.skip()
                 item.funcargs[replace_arg] = load_dataset(tds)
 
         yield item
