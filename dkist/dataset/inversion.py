7f13525ad394fba43bbe6a9ae3936e9adb83a7a3
Drew Leonard <andy.j.leonard@gmail.com>
2025-05-22 14:43:54 +0100

    Add plotting helper for profiles

--- dkist/dataset/inversion.py
+++ dkist/dataset/inversion.py
@@ -1,5 +1,12 @@
 import copy
+import types
 import textwrap
+from collections.abc import Iterable
+
+import matplotlib.figure
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.gridspec import GridSpec
 
 import asdf
 
@@ -72,3 +79,62 @@ class Inversion(NDCollection):
         """
 
         return textwrap.dedent(s).format(quants_repr, profiles_repr)
+
+    def plot_profiles(
+        self,
+        slice_index: int | slice | Iterable[int | slice],
+        share_zscale: bool = False,
+        figure: matplotlib.figure.Figure | None = None,
+        **kwargs
+    ):
+        """
+        Plot a slice of each tile in the TiledDataset
+
+        Parameters
+        ----------
+        slice_index
+            Object representing a slice which will reduce each component dataset
+            of the TiledDataset to a 2D image. This is passed to
+            `.TiledDataset.slice_tiles`, if each tile is already 2D pass ``slice_index=...``.
+        share_zscale
+            Determines whether the y-axis scale of the plots should be calculated
+            independently (``False``) or shared across all plots (``True``).
+            Defaults to False
+        figure
+            A figure to use for the plot. If not specified the current pyplot
+            figure will be used, or a new one created.
+        """
+        if isinstance(slice_index, (int, slice, types.EllipsisType)):
+            slice_index = (slice_index,)
+
+        vmin, vmax = np.inf, 0
+
+        if figure is None:
+            figure = plt.gcf()
+
+        sliced_profiles = self.profiles[slice_index]
+        lines = [k for k in sliced_profiles.keys() if "_fit" not in k]
+        ncols, nrows = len(lines), 4
+        gridspec = GridSpec(nrows=nrows, ncols=ncols, figure=figure)
+        for l, line in enumerate(lines):
+            for s, stokes in enumerate(["I", "Q", "U", "V"]):
+                profile = sliced_profiles[line][..., s]
+                fit = sliced_profiles[line+"_fit"][..., s]
+
+                ax_gridspec = gridspec[s, l]
+                ax = figure.add_subplot(ax_gridspec, projection=profile)
+
+                profile.plot(axes=ax, marker="o", linestyle="", **kwargs)
+                fit.plot(axes=ax, **kwargs)
+
+                xlabel = ax.get_xlabel()
+                ax.set_ylabel(" ")
+                ax.set_xlabel(" ")
+                if l == 0:
+                    ax.set_ylabel(stokes)
+                if s == 0:
+                    ax.set_title(line)
+                if s == 3:
+                    ax.set_xlabel(xlabel)
+
+        return figure
