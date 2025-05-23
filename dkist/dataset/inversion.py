2cdb8eef2d4f90eea4287809a9c45eecc1b9e310
Drew Leonard <andy.j.leonard@gmail.com>
2025-05-23 13:11:01 +0100

    Undo this because it wasn't helping

--- dkist/dataset/inversion.py
+++ dkist/dataset/inversion.py
@@ -62,7 +62,7 @@ class Inversion(NDCollection):
         return cls(newtree["inversion"]["quantities"].items(), aligned_axes="all", profiles=profiles)
 
     def __init__(self, *args, profiles=None, **kwargs):
-        super().__init__(args[0], *args[1:], **kwargs)
+        super().__init__(*args, **kwargs)
         self.profiles = profiles
 
     def __str__(self):
