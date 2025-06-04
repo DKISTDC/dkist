aaf536f3e1f99f66f83040ceb2562a9c8512ad09
Drew Leonard <andy.j.leonard@gmail.com>
2025-06-04 10:10:11 +0100

    Make Profile class for neater plotting
    And probably some other benefits down the line

--- dkist/dataset/inversion.py
+++ dkist/dataset/inversion.py
@@ -17,6 +17,67 @@ from dkist import Dataset
 __all__ = ["Inversion"]
 
 
+class Profile(NDCollection):
+    def plot(
+        self,
+        slice_index: int | slice | Iterable[int | slice],
+        share_zscale: bool = False,
+        figure: matplotlib.figure.Figure | None = None,
+        **kwargs
+    ):
+        """
+        Plot a slice of each tile in the TiledDataset
+
+        Parameters
+        ----------
+        slice_index
+            Object representing a slice which will reduce each component dataset
+            of the TiledDataset to a 2D image. This is passed to
+            `.TiledDataset.slice_tiles`, if each tile is already 2D pass ``slice_index=...``.
+        share_zscale
+            Determines whether the y-axis scale of the plots should be calculated
+            independently (``False``) or shared across all plots (``True``).
+            Defaults to False
+        figure
+            A figure to use for the plot. If not specified the current pyplot
+            figure will be used, or a new one created.
+        """
+        if isinstance(slice_index, (int, slice, types.EllipsisType)):
+            slice_index = (slice_index,)
+
+        vmin, vmax = np.inf, 0
+
+        if figure is None:
+            figure = plt.gcf()
+
+        sliced_profiles = self[slice_index]
+        lines = [k for k in sliced_profiles.keys() if "_fit" not in k]
+        ncols, nrows = len(lines), 4
+        gridspec = GridSpec(nrows=nrows, ncols=ncols, figure=figure)
+        for l, line in enumerate(lines):
+            for s, stokes in enumerate(["I", "Q", "U", "V"]):
+                profile = sliced_profiles[line][..., s]
+                fit = sliced_profiles[line+"_fit"][..., s]
+
+                ax_gridspec = gridspec[s, l]
+                ax = figure.add_subplot(ax_gridspec, projection=profile)
+
+                profile.plot(axes=ax, marker="o", linestyle="", **kwargs)
+                fit.plot(axes=ax, **kwargs)
+
+                xlabel = ax.get_xlabel()
+                ax.set_ylabel(" ")
+                ax.set_xlabel(" ")
+                if l == 0:
+                    ax.set_ylabel(stokes)
+                if s == 0:
+                    ax.set_title(line)
+                if s == 3:
+                    ax.set_xlabel(xlabel)
+
+        return figure
+
+
 class Inversion(NDCollection):
     @classmethod
     def from_test_asdf(cls, asdf_file, *args, **kwargs):
@@ -57,7 +118,7 @@ class Inversion(NDCollection):
                 profiles[k] = v
             for k, v in old_profiles["fit"].items():
                 profiles[k+"_fit"] = v
-            profiles = NDCollection(profiles.items(), aligned_axes=(0, 1, 3))
+            profiles = Profile(profiles.items(), aligned_axes=(0, 1, 3))
 
         return cls(newtree["inversion"]["quantities"].items(), aligned_axes="all", profiles=profiles)
 
@@ -86,66 +147,7 @@ class Inversion(NDCollection):
 
         return new_inv
 
-    def plot_profiles(
-        self,
-        slice_index: int | slice | Iterable[int | slice],
-        share_zscale: bool = False,
-        figure: matplotlib.figure.Figure | None = None,
-        **kwargs
-    ):
-        """
-        Plot a slice of each tile in the TiledDataset
-
-        Parameters
-        ----------
-        slice_index
-            Object representing a slice which will reduce each component dataset
-            of the TiledDataset to a 2D image. This is passed to
-            `.TiledDataset.slice_tiles`, if each tile is already 2D pass ``slice_index=...``.
-        share_zscale
-            Determines whether the y-axis scale of the plots should be calculated
-            independently (``False``) or shared across all plots (``True``).
-            Defaults to False
-        figure
-            A figure to use for the plot. If not specified the current pyplot
-            figure will be used, or a new one created.
-        """
-        if isinstance(slice_index, (int, slice, types.EllipsisType)):
-            slice_index = (slice_index,)
-
-        vmin, vmax = np.inf, 0
-
-        if figure is None:
-            figure = plt.gcf()
-
-        sliced_profiles = self.profiles[slice_index]
-        lines = [k for k in sliced_profiles.keys() if "_fit" not in k]
-        ncols, nrows = len(lines), 4
-        gridspec = GridSpec(nrows=nrows, ncols=ncols, figure=figure)
-        for l, line in enumerate(lines):
-            for s, stokes in enumerate(["I", "Q", "U", "V"]):
-                profile = sliced_profiles[line][..., s]
-                fit = sliced_profiles[line+"_fit"][..., s]
-
-                ax_gridspec = gridspec[s, l]
-                ax = figure.add_subplot(ax_gridspec, projection=profile)
-
-                profile.plot(axes=ax, marker="o", linestyle="", **kwargs)
-                fit.plot(axes=ax, **kwargs)
-
-                xlabel = ax.get_xlabel()
-                ax.set_ylabel(" ")
-                ax.set_xlabel(" ")
-                if l == 0:
-                    ax.set_ylabel(stokes)
-                if s == 0:
-                    ax.set_title(line)
-                if s == 3:
-                    ax.set_xlabel(xlabel)
-
-        return figure
-
-    def plot_inversions(
+    def plot(
         self,
         slice_index: int | slice | Iterable[int | slice],
         figure: matplotlib.figure.Figure | None = None,
