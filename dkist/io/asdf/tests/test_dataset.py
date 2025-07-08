b268d1693089bb7f1f6f119d93d7bcef155dcab4
Stuart Mumford <stuart@cadair.com>
2025-07-08 11:22:10 +0100

    Update dkist/io/asdf/tests/test_dataset.py

--- dkist/io/asdf/tests/test_dataset.py
+++ dkist/io/asdf/tests/test_dataset.py
@@ -38,11 +38,8 @@ def test_roundtrip_file_manager(file_manager):
 def assert_dataset_equal(new, old):
     old_headers = old.meta.pop("headers")
     new_headers = new.meta.pop("headers")
-    if isinstance(old_headers, dict):
-        assert old_headers == new_headers
-    else:
-        assert old_headers.colnames == new_headers.colnames
-        assert len(old_headers) == len(new_headers)
+    assert old_headers.colnames == new_headers.colnames
+    assert len(old_headers) == len(new_headers)
     assert old.meta == new.meta
     old.meta["headers"] = old_headers
     new.meta["headers"] = new_headers
