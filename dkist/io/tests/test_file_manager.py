c5d4d8dd0bb463de492e5021be1913639cf0e5cd
Drew Leonard <andy.j.leonard@gmail.com>
2025-07-09 14:42:54 +0100

    Forgot to update the tests

--- dkist/io/tests/test_file_manager.py
+++ dkist/io/tests/test_file_manager.py
@@ -235,8 +235,10 @@ def test_tiled_file_manager_download(large_tiled_dataset, orchestrate_transfer_m
 
 
 def test_dkist_file_manager_repr(large_visp_dataset):
-    assert "DKISTFileManager containing 80 files with each array having shape (1, 50, 128)" in repr(large_visp_dataset.files)
+    assert "DKISTFileManager containing 80 files stored in" in repr(large_visp_dataset.files)
+    assert "Each file array has shape (1, 50, 128)" in repr(large_visp_dataset.files)
 
 
 def test_tiled_dataset_file_manager_repr(large_tiled_dataset):
-    assert f"DKISTFileManager containing {(9-large_tiled_dataset.mask.sum())*3} files with each array having shape (1, 4096, 4096)" in repr(large_tiled_dataset.files)
+    assert f"DKISTFileManager containing {(9-large_tiled_dataset.mask.sum())*3} files stored in" in repr(large_tiled_dataset.files)
+    assert "Each file array has shape (1, 4096, 4096)" in repr(large_tiled_dataset.files)
