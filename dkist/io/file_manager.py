75f0849df27016be807a848779a244ef388f9d09
Drew Leonard <andy.j.leonard@gmail.com>
2025-07-09 14:09:45 +0100

    Reword FileManager repr

--- dkist/io/file_manager.py
+++ dkist/io/file_manager.py
@@ -7,6 +7,7 @@ import json
 import urllib
 from typing import Any, Protocol
 from pathlib import Path
+from textwrap import dedent
 
 from parfive import Downloader, Results
 
@@ -44,7 +45,10 @@ class DKISTFileManager:
     __slots__ = ["_fm", "_inventory_cache", "_ndcube"]
 
     def __str__(self) -> str:
-        return f"DKISTFileManager containing {len(self)} files with each array having shape {self.shape}"
+        return dedent(f"""
+            DKISTFileManager containing {len(self)} files stored in {self.basepath}.
+            Each file array has shape {self.shape}.
+        """)
 
     def __repr__(self) -> str:
         return f"{object.__repr__(self)}\n{self}"
