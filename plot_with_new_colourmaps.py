d51cd181da369f9d3ff5094371aaab3678353a60
Drew Leonard <andy.j.leonard@gmail.com>
2025-08-22 14:13:19 +0100

    Assorted changes

--- plot_with_new_colourmaps.py
+++ plot_with_new_colourmaps.py
@@ -1,19 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from dkist import load_dataset
-from dkist.data.sample import VISP_L1_KMUPT, VBI_L1_NZJTB, CRYO_L1_TJKGC, CRYO_L1_MSCGD
 from pathlib import Path
-from astropy.coordinates import SpectralCoord
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
 import astropy.units as u
+from astropy.coordinates import SpectralCoord
+
+from dkist import load_dataset
+from dkist.data.sample import CRYO_L1_TJKGC
+
 
 def cmap_from_csv(fname):
-    cmapvals = np.loadtxt(fname, skiprows=1, delimiter=',', comments=None, dtype=str)
+    cmapvals = np.loadtxt(fname, skiprows=1, delimiter=",", comments=None, dtype=str)
 
-    colourname = f.name[5:].strip('.csv')
+    colourname = f.name[5:].strip(".csv")
     return ListedColormap(cmapvals[:, 1], name=colourname)
 
-cmap_files = Path('.').glob('cmap-*.csv')
+cmap_files = Path().glob("cmap-*.csv")
 
 # for dsfile in [VBI_L1_NZJTB]:
 #     ds = load_dataset(dsfile)
@@ -31,17 +35,16 @@ cmap_files = Path('.').glob('cmap-*.csv')
 #         plt.savefig(f'colormap-tests/{instrument}_{cmap.name}.png')
 #     plt.close()
 
-for dsfile in [VISP_L1_KMUPT, CRYO_L1_TJKGC]: #, CRYO_L1_MSCGD]:
-# for dsfile in [CRYO_L1_TJKGC]:
+# for dsfile in [VISP_L1_KMUPT, CRYO_L1_TJKGC]: #, CRYO_L1_MSCGD]:
+for dsfile in [CRYO_L1_TJKGC]:
     ds = load_dataset(dsfile)
-    instrument = ds.headers[0]['INSTRUME']
-    peak_idx = ds.rebin((-1, -1, 1), function=np.sum).squeeze().wcs.world_to_array_index(SpectralCoord(1079.75, unit=u.nm))
-    datmean = ds.data.mean(axis=2)
-    if instrument == 'VISP':
+    instrument = ds.headers[0]["INSTRUME"]
+    if instrument == "VISP":
         # continue
         ds = ds[0, :, 0]
     else:
-        continue
+        peak_idx = ds.rebin((-1, -1, 1), function=np.sum).squeeze().wcs.world_to_array_index(SpectralCoord(1079.75, unit=u.nm))
+        datmean = ds.data.mean(axis=2)
         ds = ds[:, :, peak_idx]
         ds.data = ds.data - datmean
     ds.data = ds.data.compute()
@@ -51,7 +54,6 @@ for dsfile in [VISP_L1_KMUPT, CRYO_L1_TJKGC]: #, CRYO_L1_MSCGD]:
         cmap = cmap_from_csv(f)
         print(f"Plotting {instrument} in {cmap.name}")
 
-        ds.plot(aspect='auto', cmap=cmap)#, vmin=np.percentile(ds.data, 2), vmax=np.percentile(ds.data, 98))
-        plt.savefig(f'colormap-tests/{instrument}_{cmap.name}.png')
+        ds.plot(aspect="auto", cmap=cmap)#, vmin=np.percentile(ds.data, 2), vmax=np.percentile(ds.data, 98))
+        plt.savefig(f"colormap-tests/{instrument}_{cmap.name}.png")
     plt.close()
-
