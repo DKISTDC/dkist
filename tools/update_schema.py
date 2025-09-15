6900460e875784d1fbeb19e85dd0a4a02c345aba
Drew Leonard <andy.j.leonard@gmail.com>
2025-09-15 13:19:36 +0100

    Find and copy latest schema version

--- tools/update_schema.py
+++ tools/update_schema.py
@@ -6,10 +6,35 @@
 This script adds and updates the appropriate files to update the version of an ASDF schema.
 """
 
-def main(schema_name, schema_increment_type="minor", manifest_increment_type="minor", base_branch="main"):
+import sys
+import shutil
+import pathlib
+import argparse
+
+
+def main(schema_name, schema_increment="minor", manifest_increment="minor", base_branch="main"):
+    repodir = pathlib.Path(__file__).parent.parent.resolve()
+    schemas_dir = repodir / "dkist" / "io" / "asdf" / "resources" / "schemas"
+    old_schemas = list(schemas_dir.glob(f"{schema_name}-*.*.*.yaml"))
+    old_schemas.sort()
     # if schema exists in base branch:
-    #   get latest schema version on base branch
-    #   copy <schema>-<latest>.yaml to <schema>-<latest+increment>.yaml
+    if old_schemas:
+        # get latest schema filename on base branch
+        old_schema_file = old_schemas[-1]
+        # get latest version number
+        old_schema_version = old_schema_file.name[-10:-5]
+        # copy <schema>-<latest>.yaml to <schema>-<latest+increment>.yaml
+        if schema_increment == "major":
+            new_schema_version = f"{int(old_schema_version[0])+1}.0.0"
+        elif schema_increment == "minor":
+            new_schema_version = f"{old_schema_version[:2]}{int(old_schema_version[2])+1}.0"
+        elif schema_increment == "bugfix":
+            new_schema_version = f"{old_schema_version[:4]}{int(old_schema_version[4])+1}"
+        else:
+            # obviously make this more sophisticated
+            raise InputError
+        new_schema_file = schemas_dir / f"{schema_name}-{new_schema_version}.yaml"
+        shutil.copy(old_schema_file, new_schema_file)
     # else:
     #   create new <schema>-0.1.0.yaml file
     # get latest manifest version on base branch
@@ -24,20 +49,21 @@ def main(schema_name, schema_increment_type="minor", manifest_increment_type="mi
     #   import new converter in converters/__init__.py
     #   import new converter in entry_points.py
     #   add converter to dkist_converters list in entry_points.py
-    pass
 
 if __name__ == "__main__":
     argp = argparse.ArgumentParser(description=__doc__)
     argp.add_argument("schema_name", help="Name of the schema to add or update.")
-    argp.add_argument("schema_increment_type", help="Increment to apply to the schema version."
+    argp.add_argument("--schema_increment", help="Increment to apply to the schema version."
                       "Ignored when creating a new schema."
                       "Must be 'major', 'minor' or 'bugfix'."
-                      "(Default 'minor')")
-    argp.add_argument("manifest_increment_type", help="Increment to apply to the manifest version."
+                      "(Default 'minor')",
+                      default="minor")
+    argp.add_argument("--manifest_increment", help="Increment to apply to the manifest version."
                       "Must be 'major', 'minor' or 'bugfix'."
-                      "(Default 'minor')")
-    argp.add_argument("base_branch", help="Branch of the git repo used to determine latest version numbers.")
+                      "(Default 'minor')",
+                      default="minor")
+    argp.add_argument("--base_branch", help="Branch of the git repo used to determine latest version numbers.", default="main")
 
     args = argp.parse_args(sys.argv[1:])
 
-    main(schema_name, args.schema_increment_type, args.manifest_increment_type, argp.base_branch)
+    main(args.schema_name, args.schema_increment, args.manifest_increment, args.base_branch)
