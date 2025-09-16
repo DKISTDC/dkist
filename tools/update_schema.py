68374be140422d9c33df5c670490ae60e1cd8c53
Drew Leonard <andy.j.leonard@gmail.com>
2025-09-16 10:42:12 +0100

    Make the manifest file and add the schema to it

--- tools/update_schema.py
+++ tools/update_schema.py
@@ -12,47 +12,76 @@ import pathlib
 import argparse
 
 
-def main(schema_name, schema_increment="minor", manifest_increment="minor", base_branch="main"):
-    repodir = pathlib.Path(__file__).parent.parent.resolve()
-    schemas_dir = repodir / "dkist" / "io" / "asdf" / "resources" / "schemas"
-    old_schemas = list(schemas_dir.glob(f"{schema_name}-*.*.*.yaml"))
-    old_schemas.sort()
-    # if schema exists in base branch:
-    if old_schemas:
-        # get latest schema filename on base branch
-        old_schema_file = old_schemas[-1]
-        # get latest version number
-        old_schema_version = old_schema_file.name[-10:-5]
-        # copy <schema>-<latest>.yaml to <schema>-<latest+increment>.yaml
-        if schema_increment == "major":
-            new_schema_version = f"{int(old_schema_version[0])+1}.0.0"
-        elif schema_increment == "minor":
-            new_schema_version = f"{old_schema_version[:2]}{int(old_schema_version[2])+1}.0"
-        elif schema_increment == "bugfix":
-            new_schema_version = f"{old_schema_version[:4]}{int(old_schema_version[4])+1}"
+def get_latest(name, dir):
+    # ? here for the major number so that it only matches one character and "dkist" doesn't match "dkist-wcs-"
+    # This will break if major version numbers exceed 9 but that's a problem for future Drew
+    old_files = list(dir.glob(f"{name}-?.*.*.yaml"))
+    old_files.sort()
+    if old_files:
+        return old_files[-1]
+
+
+def increment_version(old_file, increment):
+    if old_file:
+        old_version = old_file.name[-10:-5]
+        if increment == "major":
+            new_version = f"{int(old_version[0])+1}.0.0"
+        elif increment == "minor":
+            new_version = f"{old_version[:2]}{int(old_version[2])+1}.0"
+        elif increment == "bugfix":
+            new_version = f"{old_version[:4]}{int(old_version[4])+1}"
         else:
             # obviously make this more sophisticated
             raise InputError
-        new_schema_file = schemas_dir / f"{schema_name}-{new_schema_version}.yaml"
-        shutil.copy(old_schema_file, new_schema_file)
-    # else:
-    #   create new <schema>-0.1.0.yaml file
-    # get latest manifest version on base branch
-    # create manifests/dkist-<latest+increment>.yaml
-    # if schema exists on base_branch:
-    #   increment schema_uri and tag_uri
-    # else:
-    #   add schema_uri and tag_uri
+        new_file = old_file.parent / old_file.name.replace(old_version, new_version)
+        shutil.copy(old_file, new_file)
+        return new_file, old_version, new_version
+    new_file = dir / f"{name}-0.1.0.yaml"
+    new_file.touch()
+    return new_file, None, "0.1.0"
+
+
+def main(schema_name, manifest="dkist", schema_increment="minor", manifest_increment="minor", base_branch="main"):
+    repodir = pathlib.Path(__file__).parent.parent.resolve()
+    asdf_dir = repodir / "dkist" / "io" / "asdf"
+
+    old_schema = get_latest(schema_name, asdf_dir / "resources" / "schemas")
+    old_manifest = get_latest(manifest, asdf_dir / "resources" / "manifests")
+
+    new_sche_file, old_sche_ver, new_sche_ver = increment_version(old_schema, schema_increment)
+    new_mani_file, old_mani_ver, new_mani_ver = increment_version(old_manifest, manifest_increment)
+
     # add ManifestExtension to entry_points.py
+    entrypoints = asdf_dir / "entry_points.py"
+    converters = "wcs" if manifest == "dkist-wcs" else "dkist"
+    with open(entrypoints) as f:
+        lines = f.readlines()
+    old_ext_line = f'        ManifestExtension.from_uri("asdf://dkist.nso.edu/manifests/{manifest}-{old_mani_ver}",\n'
+    new_ext_line = old_ext_line[:-1].replace(old_mani_ver, new_mani_ver) + f" converters='{converters}'),\n"
+    linenum = lines.index(old_ext_line)
+    lines.insert(linenum, new_ext_line)
+    with open(entrypoints, mode="w") as f:
+        f.write("".join(lines))
+    # if schema exists on base_branch:
+    if old_schema:
+        #   increment schema_uri and tag_uri
+        pass
     # if !(schema exists) on base branch:
-    #   create converters/<schema>.py skeleton
-    #   import new converter in converters/__init__.py
-    #   import new converter in entry_points.py
-    #   add converter to dkist_converters list in entry_points.py
+    else:
+        #   add schema_uri and tag_uri
+        #   create converters/<schema>.py skeleton
+        #   import new converter in converters/__init__.py
+        #   import new converter in entry_points.py
+        #   add converter to dkist_converters list in entry_points.py
+        pass
 
 if __name__ == "__main__":
     argp = argparse.ArgumentParser(description=__doc__)
     argp.add_argument("schema_name", help="Name of the schema to add or update.")
+    argp.add_argument("--manifest", help="Name of the manifest to add the new schema to."
+                      "Creating a new manifest is not currently supported."
+                      "(Default 'dkist')",
+                      default="dkist")
     argp.add_argument("--schema_increment", help="Increment to apply to the schema version."
                       "Ignored when creating a new schema."
                       "Must be 'major', 'minor' or 'bugfix'."
@@ -66,4 +95,4 @@ if __name__ == "__main__":
 
     args = argp.parse_args(sys.argv[1:])
 
-    main(args.schema_name, args.schema_increment, args.manifest_increment, args.base_branch)
+    main(args.schema_name, args.manifest, args.schema_increment, args.manifest_increment, args.base_branch)
