32b0f78fa321527a65477d3200aefa4600a0278b
Drew Leonard <andy.j.leonard@gmail.com>
2025-09-23 16:15:35 +0100

    Let's see if this helps

--- dkist/io/__init__.py
+++ dkist/io/__init__.py
@@ -1,6 +1,7 @@
 """
 Functionality for loading many DKIST FITS files into a single Dask array.
 """
+from . import utils
 from .file_manager import DKISTFileManager
 
 __all__ = ["DKISTFileManager"]
