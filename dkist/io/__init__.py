d963feec9fd1699a6159ef696d6ff394dbffb067
Drew Leonard <andy.j.leonard@gmail.com>
2025-09-24 09:49:44 +0100

    Who even knows what's going on at this point

--- dkist/io/__init__.py
+++ dkist/io/__init__.py
@@ -2,6 +2,5 @@
 Functionality for loading many DKIST FITS files into a single Dask array.
 """
 from .file_manager import DKISTFileManager
-from .utils import filemanager_info_str
 
 __all__ = ["DKISTFileManager"]
