0d04596b7e7edf126ed5ec02271950e548d8884f
Drew Leonard <andy.j.leonard@gmail.com>
2025-10-02 13:42:23 +0100

    Refactor

--- dkist/io/asdf/tests/test_schema_infra.py
+++ dkist/io/asdf/tests/test_schema_infra.py
@@ -12,20 +12,14 @@ def tagname(fulltag):
     return fulltag.split("/")[-1]
 
 
-def test_schema_infrastructure():
-
+def get_infra_info():
     man_yamls = itertools.groupby(
         sorted((repodir / "io" / "asdf" / "resources" / "manifests").glob("*")), lambda x: tagname(x)[:-11]
     )
     latest_man_yamls = {man: sorted(yamls)[-1] for man, yamls in man_yamls}
     latest_extensions = {ext: list(manifests)[-1] for ext, manifests in itertools.groupby(sorted(entry_points.get_extensions(), key=lambda x: x.extension_uri),
                                                                                          lambda x: tagname(x.extension_uri)[:-6])}
-    # Check that latest manifest.yaml == latest listed in entry_points
-    for man, yaml in latest_man_yamls.items():
-        man_yaml_ver = yaml.name[-10:-5]
-        assert man_yaml_ver == tagname(latest_extensions[man].extension_uri)[-5:]
 
-    schemas = [tagname(tag.tag_uri) for ext in latest_extensions.values() for tag in ext.tags]
     converters = [type(conv) for ext in latest_extensions.values() for conv in ext.converters]
     schema_yamls = itertools.groupby(
         sorted((repodir / "io" / "asdf" / "resources" / "schemas").glob("*")), lambda x: tagname(x)[:-11]
@@ -38,16 +32,53 @@ def test_schema_infrastructure():
             for tag in schema_uris:
                 latest_schemas_in_manifest[tagname(tag)[:-6]] = tag[-5:]
 
-    for schema, yaml in latest_sche_yamls.items():
-        yaml_version = yaml.name[-10:-5]
-        converter = "".join([p.title() for p in schema.replace("_model", "").replace("_transform", "").split("_")]) + "Converter"
-        # Check schema converter is imported in entry_points
-        assert hasattr(entry_points, converter)
-        # Check schema is in [dkist|wcs]_converters in entry_points
-        assert eval(f"dkist.io.asdf.converters.{converter}") in converters
-        # Check that latest schema.yaml version == latest listed in manifest
-        assert yaml_version == latest_schemas_in_manifest[schema]
+    return latest_man_yamls, latest_extensions, latest_sche_yamls, converters, latest_schemas_in_manifest
+
+
+def get_schema_info(schema, yaml):
+    yaml_version = yaml.name[-10:-5]
+    converter = "".join([p.title() for p in schema.replace("_model", "").replace("_transform", "").split("_")]) + "Converter"
+    return yaml_version, converter
+
+
+def manifest_yaml_versions_match_entry_points(latest_man_yamls, latest_extensions):
+    # Check that latest manifest.yaml == latest listed in entry_points
+    for man, yaml in latest_man_yamls.items():
+        man_yaml_ver = yaml.name[-10:-5]
+        return man_yaml_ver == tagname(latest_extensions[man].extension_uri)[-5:]
+
+
+def schema_converter_imported(converter):
+    # Check schema converter is imported in entry_points
+    return hasattr(entry_points, converter)
+
+
+def schema_in_converters_list(converter, converters):
+    # Check schema is in [dkist|wcs]_converters in entry_points
+    return eval(f"dkist.io.asdf.converters.{converter}") in converters
+
+
+def schema_yaml_matches_manifest(yaml_version, latest_schemas_in_manifest):
+    # Check that latest schema.yaml version == latest listed in manifest
+    return yaml_version == latest_schemas_in_manifest
+
+
+def schema_ver_matches_filename(yaml, yaml_version):
         # Check that schema version in schema matches filename
         with open(yaml) as f:
             schema_version_in_yaml = f.readlines()[3][-7:-2]
-        assert schema_version_in_yaml == yaml_version
+        return schema_version_in_yaml == yaml_version
+
+
+def test_schema_infrastructure():
+    latest_man_yamls, latest_extensions, latest_sche_yamls, converters, latest_schemas_in_manifest = get_infra_info()
+
+    assert manifest_yaml_versions_match_entry_points(latest_man_yamls, latest_extensions)
+
+    # schemas = [tagname(tag.tag_uri) for ext in latest_extensions.values() for tag in ext.tags]
+    for schema, yaml in latest_sche_yamls.items():
+        yaml_version, converter = get_schema_info(schema, yaml)
+        assert schema_converter_imported(converter)
+        assert schema_in_converters_list(converter, converters)
+        assert schema_yaml_matches_manifest(yaml_version, latest_schemas_in_manifest[schema])
+        assert schema_ver_matches_filename(yaml, yaml_version)
