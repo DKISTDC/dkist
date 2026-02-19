dd6df868aac47b7778537c7c8a9902520015d3ee
Stuart Mumford <stuart@cadair.com>
2026-02-19 14:01:18 +0000

    Add str and repr with tests

--- dkist/dataset/tests/test_extras.py
+++ dkist/dataset/tests/test_extras.py
@@ -25,3 +25,21 @@ def test_data(dataset_extra_obj, dataset_extra_dir):
         dask_arr = arr.compute()
         fits_arr = fits.getdata(dataset_extra_dir / ear.fileuri, hdu=ear.target)
         np.testing.assert_allclose(dask_arr, fits_arr)
+
+
+def test_str_same_shapes(dataset_extra_obj):
+    result = str(dataset_extra_obj)
+    assert result == "DatasetExtra<name=test_extra, length=2, shape=(10, 10)>"
+
+
+def test_str_different_shapes(dataset_extra_obj):
+    extra = dataset_extra_obj
+    extra._ears[1].shape = (20, 20)
+    result = str(extra)
+    assert result == "DatasetExtra<name=test_extra, length=2, shape=((10, 10), (20, 20))>"
+
+
+def test_repr(dataset_extra_obj):
+    result = repr(dataset_extra_obj)
+    assert result.startswith("<DatasetExtra<name=test_extra, length=2, shape=(10, 10)> object at 0x")
+    assert result.endswith(">")
