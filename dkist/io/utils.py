8f1ff88359167a9eef09f920490502472f2f423c
Drew Leonard <andy.j.leonard@gmail.com>
2026-03-30 13:34:47 +0100

    Fix indentation

--- dkist/io/utils.py
+++ dkist/io/utils.py
@@ -15,30 +15,30 @@ def filemanager_info_str(filemanager):
 
 
 def save_dataset(dataset, asdf_path, overwrite=False):
-        """
-        Write a DKIST dataset to an ASDF file
+    """
+    Write a DKIST dataset to an ASDF file
 
-        This function takes a `dkist.Dataset`, `dkist.TiledDataset` or
-        `dkist.Inversion` object and saves it to a new file.
+    This function takes a `dkist.Dataset`, `dkist.TiledDataset` or
+    `dkist.Inversion` object and saves it to a new file.
 
-        Parameters
-        ----------
-        dataset : `dkist.Dataset`, `dkist.TiledDataset` or `dkist.Inversion`
-            Level-1 or -2 dataset object to save
+    Parameters
+    ----------
+    dataset : `dkist.Dataset`, `dkist.TiledDataset` or `dkist.Inversion`
+        Level-1 or -2 dataset object to save
 
-        asdf_path : Path or str
-            Location to save the dataset to
+    asdf_path : Path or str
+        Location to save the dataset to
 
-        overwrite : bool
-            If `True`, overwrites the existing file at `asdf_path`, otherwise raises `FileExistsError`.
-            Default `False`.
-        """
-        from dkist.dataset import Inversion  # noqa: PLC0415
+    overwrite : bool
+        If `True`, overwrites the existing file at `asdf_path`, otherwise raises `FileExistsError`.
+        Default `False`.
+    """
+    from dkist.dataset import Inversion  # noqa: PLC0415
 
-        if isinstance(asdf_path, str):
-            asdf_path = Path(asdf_path)
+    if isinstance(asdf_path, str):
+        asdf_path = Path(asdf_path)
 
-        if not overwrite and asdf_path.exists():
-            raise FileExistsError(f"ASDF file {asdf_path} already exists. Use overwrite=True to replace it.")
+    if not overwrite and asdf_path.exists():
+        raise FileExistsError(f"ASDF file {asdf_path} already exists. Use overwrite=True to replace it.")
 
-        asdf.AsdfFile({"inversion" if isinstance(dataset, Inversion) else "dataset": dataset}).write_to(asdf_path)
+    asdf.AsdfFile({"inversion" if isinstance(dataset, Inversion) else "dataset": dataset}).write_to(asdf_path)
