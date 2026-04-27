95c697c176656e083f3442e276e2e0b9a5d723eb
Drew Leonard <andy.j.leonard@gmail.com>
2026-04-27 11:23:10 +0100

    More and better stuff for saving to single asdf

--- dkist/io/utils.py
+++ dkist/io/utils.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from textwrap import dedent
 
+import zarr
+
 import asdf
 
 __all__ = ["filemanager_info_str", "save_dataset"]
@@ -14,7 +16,7 @@ def filemanager_info_str(filemanager):
     """)
 
 
-def save_dataset(dataset, asdf_path, overwrite=False):
+def save_dataset(dataset, asdf_path, overwrite=False, data_format="external"):
     """
     Write a DKIST dataset to an ASDF file
 
@@ -32,14 +34,21 @@ def save_dataset(dataset, asdf_path, overwrite=False):
     overwrite : bool
         If `True`, overwrites the existing file at `asdf_path`, otherwise raises `FileExistsError`.
         Default `False`.
+
+    data_format : str, default="fits"
+        Determines how the data array is represented in the saved asdf file.
+        If "external", the asdf file will contain references to external fits files, as in asdf
+        files distributed by the DKIST Data Center.
+        If "internal", data will be saved as binary blocks within the asdf file itself.
     """
     from dkist.dataset import Inversion  # noqa: PLC0415
 
     if isinstance(asdf_path, str):
         asdf_path = Path(asdf_path)
-    if asdf_path.suffix() == "asdf":
-        newdata = zarr.storage.MemoryStore()
-        dataset.data.to_zarr(newdata)
+    if data_format == "internal":
+        store = zarr.storage.MemoryStore({})
+        newdata = zarr.create_array(store=store, data=dataset.data)
+        dataset.data = newdata
 
     if not overwrite and asdf_path.exists():
         raise FileExistsError(f"ASDF file {asdf_path} already exists. Use overwrite=True to replace it.")
